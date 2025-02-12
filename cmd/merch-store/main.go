33faf991aaf79158f292be027eeab106a3186c98
justcgh9 <a.kabardiyadi@innopolis.university>
2025-02-12 14:41:05 +0300

    approximate main

--- cmd/merch-store/main.go
+++ cmd/merch-store/main.go
@@ -1,9 +1,79 @@
 package main
 
-import "github.com/justcgh9/merch_store/internal/config"
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/justcgh9/merch_store/internal/config"
+	"github.com/justcgh9/merch_store/internal/http-server/handlers/auth"
+	mySlog "github.com/justcgh9/merch_store/internal/log"
+	"github.com/justcgh9/merch_store/internal/storage/postgres"
+)
 
 func main() {
+
 	cfg := config.MustLoad()
 
-	_ = cfg
+	log := mySlog.SetupLogger(cfg.Env)
+
+	log.Info("starting merch store", slog.String("env", cfg.Env))
+
+	storage := postgres.New(cfg.StoragePath)
+	defer func() {
+		err := storage.Close()
+		if err != nil {
+			log.Error("error closing db connection", slog.String("err", err.Error()))
+		}
+	}()
+
+	log.Info("connected to postgres")
+
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	router.Post("/api/auth", auth.New())
+
+	srv := &http.Server{
+		Addr: cfg.Address,
+		Handler: router,
+		ReadTimeout: cfg.Timeout,
+		WriteTimeout: cfg.Timeout,
+		IdleTimeout: cfg.IddleTimeout,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Error("failed to start server")
+		}
+	}()
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	log.Info("server started")
+
+	<-done
+	log.Info("stopping server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), cfg.Timeout)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Error("failed to stop server", slog.String("err", err.Error()))
+
+		return
+	}
+
+	log.Info("server stopped")
 }
--- internal/storage/postgres/postgres.go
+++ internal/storage/postgres/postgres.go
@@ -1,1 +1,39 @@
 package postgres
+
+import (
+	"context"
+	"log"
+
+	"github.com/jackc/pgx"
+)
+
+type Storage struct {
+	conn *pgx.Conn
+}
+
+func New(connString string) *Storage {
+	const op = "storage.postgres.New"
+
+	config, err := pgx.ParseURI(connString)
+	if err != nil {
+		log.Fatalf("%s %v", op, err)
+	}
+
+	conn, err := pgx.Connect(config)
+	if err != nil {
+		log.Fatalf("%s %v", op, err)
+	}
+
+	err = conn.Ping(context.Background())
+	if err != nil {
+		log.Fatalf("%s %v", op, err)
+	}
+
+	return &Storage{
+		conn: conn,
+	}
+}
+
+func (s *Storage) Close() error {
+	return s.conn.Close()
+}
