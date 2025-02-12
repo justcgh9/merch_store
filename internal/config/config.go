b176850d2497eab403c7df66e63fb79f1070c444
justcgh9 <a.kabardiyadi@innopolis.university>
2025-02-12 14:41:05 +0300

    approximate main

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,7 +10,7 @@ import (
 
 type Config struct {
 	Env         string `yaml:"env" env-default:"local"`
-	StoragePath string `yaml:"storage_path" env-required:"true"`
+	StoragePath string `yaml:"storage" env-required:"true"`
 	HttpServer  `yaml:"http_server"`
 }
 
--- internal/http-server/handlers/auth/auth.go
+++ internal/http-server/handlers/auth/auth.go
@@ -1,1 +1,21 @@
 package auth
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+type AuthRequest struct {
+	Username string `json:"username" validate:"required,alphanum"`
+	Password string `json:"password" validate:"required,aplhanum"`
+}
+
+type Authenticator interface {
+
+}
+
+func New(log *slog.Logger, authenticator Authenticator) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
