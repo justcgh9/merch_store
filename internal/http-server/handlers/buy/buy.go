9f3e462714e2365a6aabeea1cffcadbc7bfba378
justcgh9 <a.kabardiyadi@innopolis.university>
2025-02-12 15:50:15 +0300

    Auth middleware

--- internal/http-server/handlers/buy/buy.go
+++ internal/http-server/handlers/buy/buy.go
@@ -3,7 +3,6 @@ package buy
 import (
 	"log/slog"
 	"net/http"
-	"strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -12,7 +11,6 @@ import (
 )
 
 type Buyer interface {
-	Authorize(token string) (user.UserDTO, error)
 	Buy(username, item string) error
 }
 
@@ -21,60 +19,40 @@ type BuyResponseError struct {
 }
 
 const (
-	itemParam = "item"
-	authHeader = "Authorization"
+	itemParam  = "item"
+	userDTOKey = "userDTO"
 )
 
 func New(log *slog.Logger, buyer Buyer) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		const op = "handlers.buy.New"
-
+		
 		log = log.With(
 			slog.String("op", op),
 			slog.String("request_id", middleware.GetReqID(r.Context())),
 		)
 
-		item := chi.URLParam(r, itemParam)
-
-		authHeader := r.Header.Get(authHeader)
-
-		if authHeader == "" {
-			log.Error("missing authorization header")
-			render.Status(r, http.StatusUnauthorized)
-			render.JSON(w, r, BuyResponseError{
-				Error: "missing authorization header",
-			})
-
-			return
-		}
-
-		if len(authHeader) < 7 || authHeader[:6] != "Bearer" {
-			log.Error("invalid authorization header")
-			render.Status(r, http.StatusUnauthorized)
-			render.JSON(w, r, BuyResponseError{
-				Error: "invalid authorization header",
-			})
-			return
-		}
-
-		userDTO, err := buyer.Authorize(strings.Split(authHeader, "Bearer ")[1])
-		if err != nil {
-			log.Error("invalid jwt token", slog.String("err", err.Error()))
-			render.Status(r, http.StatusUnauthorized)
-			render.JSON(w, r, BuyResponseError{
-				Error: "invalid jwt token",
-			})
-			return
-		}
+		userDTO, ok := r.Context().Value(userDTOKey).(user.UserDTO)
 
 		log = log.With(
 			slog.String("username", userDTO.Username),
 		)
 
-		err = buyer.Buy(userDTO.Username, item)
+		item := chi.URLParam(r, itemParam)
+
+		if !ok {
+			log.Error("could not get user info")
+			render.Status(r, http.StatusUnauthorized)
+			render.JSON(w, r, BuyResponseError{
+				Error: "could not get user info",
+			})
+			return
+		}
+
+		err := buyer.Buy(userDTO.Username, item)
 		if err != nil {
 			// TODO distinguish the errors
-			log.Error("could not buy " + item, slog.String("err", err.Error()))
+			log.Error("could not buy "+item, slog.String("err", err.Error()))
 			render.Status(r, http.StatusBadRequest)
 			render.JSON(w, r, BuyResponseError{
 				Error: "could not buy " + item,
