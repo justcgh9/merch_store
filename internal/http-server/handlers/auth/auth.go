fd6a23be4ab31af9b7b8f838b782e727112e0026
justcgh9 <a.kabardiyadi@innopolis.university>
2025-02-12 15:09:18 +0300

    auth handler

--- internal/http-server/handlers/auth/auth.go
+++ internal/http-server/handlers/auth/auth.go
@@ -3,19 +3,82 @@ package auth
 import (
 	"log/slog"
 	"net/http"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
 )
+
+type Authenticator interface {
+	Authenticate(username, password string) (string, error)
+}
 
 type AuthRequest struct {
 	Username string `json:"username" validate:"required,alphanum"`
 	Password string `json:"password" validate:"required,aplhanum"`
 }
 
-type Authenticator interface {
+type AuthResponseOK struct {
+	Token string `json:"token"`
+}
 
+type AuthResponseError struct {
+	Error string `json:"errors"`
 }
 
 func New(log *slog.Logger, authenticator Authenticator) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.auth.New"
 
+		log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		var req AuthRequest
+
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("error decoding request body", slog.String("err", err.Error()))
+			render.Status(r, http.StatusBadRequest)
+
+			render.JSON(w, r, AuthResponseError{
+				Error: "error decoding request body: " + err.Error(),
+			})
+		}
+
+		if err := validator.New().Struct(req); err != nil {
+			validateErr := err.(validator.ValidationErrors)
+
+			log.Error("invalid request", slog.String("err", err.Error()))
+
+			render.Status(r, http.StatusBadRequest)
+
+			render.JSON(w, r, AuthResponseError{
+				Error: validateErr.Error(),
+			})
+
+			return
+		}
+
+		token, err := authenticator.Authenticate(req.Username, req.Password)
+		if err != nil {
+			
+			log.Error("error authenticating user", slog.String("err", err.Error()))
+
+			render.Status(r, http.StatusUnauthorized)
+
+			render.JSON(w, r, AuthResponseError{
+				Error: err.Error(),
+			})
+
+			return
+		}
+
+		render.Status(r, http.StatusOK)
+		render.JSON(w, r, AuthResponseOK{
+			Token: token,
+		})
+		return
 	}
 }
